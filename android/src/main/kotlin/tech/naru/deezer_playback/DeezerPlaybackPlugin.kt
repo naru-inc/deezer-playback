908669e473b64ecd16134bf3957c9ba42c4459eb
marwenbadri <marwenbadri@gmail.com>
2019-10-16 11:19:35 +0100

    views

--- android/src/main/kotlin/tech/naru/deezer_playback/DeezerPlaybackPlugin.kt
+++ android/src/main/kotlin/tech/naru/deezer_playback/DeezerPlaybackPlugin.kt
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.util.Log
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.TextView;
-import android.widget.Toast;
 import com.deezer.sdk.model.Track
 import com.deezer.sdk.network.connect.DeezerConnect
 import com.deezer.sdk.network.connect.event.DialogError
